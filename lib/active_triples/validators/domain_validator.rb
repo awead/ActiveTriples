7fdff27026c76c8493ea66e8282ce0819d30191b
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-09-04 12:32:29 -0700

    WIP: Adding domain validator; removing pry blocks

--- lib/active_triples/validators/domain_validator.rb
+++ lib/active_triples/validators/domain_validator.rb
@@ -7,8 +7,6 @@ module ActiveTriples::Validators
 
     def validate_each(record, attribute, values)
       predicate = record.class.properties[attribute][:predicate]
-      require 'pry'
-      binding.pry if record.type.first != RDF::DC.BibliographicResource
       record.errors.add(attribute, "Resource is of invalid type for domain restrictions on #{predicate.to_s}).") unless RDF::Vocabulary.find_term(predicate).domain_compatible?(record.rdf_subject, record)
     end
   end
